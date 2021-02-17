e344817c2e49da8d7a54f3f4f08d6c1b2be74cdc
KCN-judu <79190965+KCN-judu@users.noreply.github.com>
2021-02-17 16:05:42 +0800

    Create compiler.cpp

---
+++ compiler.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+ int main (){
+ 	system("javac JavaRun.java");
+ 	system("java JavaRun");
+ 	cout<<"This Java Project has been successfully compiled"<<endl;
+ 	cin.get();
+ 	return 0;
+ 	//designed by KCN JuDu 
+}
