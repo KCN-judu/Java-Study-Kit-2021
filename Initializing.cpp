c92e11c69b08961269d3c6a7887ddb6c705249be
KCN-judu <79190965+KCN-judu@users.noreply.github.com>
2021-02-17 16:06:36 +0800

    Create Initializing.cpp

---
+++ Initializing.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main(){
+	system("copy /-Y JavaRun.java C:\\Users\\Administrator\\Desktop\\JavaProjects"); 	
+    return 0;
+	//designed by KCN JuDu 
+} 
